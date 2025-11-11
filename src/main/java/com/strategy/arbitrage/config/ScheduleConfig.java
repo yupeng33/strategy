329b50b0df25cb2305d78a2600fccf129a3810b1
NormanWei <norman.wei@hytechc.com>
2025-11-11 10:09:45 +0800

    系统线程池bean注入

--- src/main/java/com/strategy/arbitrage/config/ScheduleConfig.java
+++ src/main/java/com/strategy/arbitrage/config/ScheduleConfig.java
@@ -1,5 +1,6 @@
 package com.strategy.arbitrage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @EnableScheduling
 public class ScheduleConfig {
 
+    @Bean
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(10); // 设置线程池大小为 10
